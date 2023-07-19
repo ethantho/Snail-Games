a18eeea68feb8e40aba4d8ba15f722993d90bd71
ethantho <115848362+ethantho@users.noreply.github.com>
2023-07-19 12:24:34 -0400

    Sprite now flips based on movement of snail

---
+++ Assets/SpriteMirroring.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteMirroring : MonoBehaviour
+{
+    Rigidbody2D rb;
+    SpriteRenderer spr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponentInParent<Rigidbody2D>();
+        spr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(rb.velocity.x < 0)
+        {
+            spr.flipX = true;
+        }
+
+        if(rb.velocity.x > 0)
+        {
+            spr.flipX = false;
+        }
+    }
+}
